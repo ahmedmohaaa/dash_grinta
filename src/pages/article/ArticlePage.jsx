83345d4892147d26134920358b25222d3514fb95
ahmedmohaaa <ma2012924@gmail.com>
2026-06-20 20:34:16 +0300

    Update ArticlePage.jsx

--- src/pages/article/ArticlePage.jsx
+++ src/pages/article/ArticlePage.jsx
@@ -64,7 +64,7 @@ const ArticlePage = () => {
   const [loading, setLoading] = useState(false);
   const [message, setMessage] = useState({ type: '', text: '' });
 
-  const API_BASE_URL = 'http://127.0.0.1:8000/api';
+  const API_BASE_URL = 'https://api.algrinta.com/api';
   const token = localStorage.getItem('access_token');
 
   // جلب المقالات عند تحميل الصفحة مباشرة
